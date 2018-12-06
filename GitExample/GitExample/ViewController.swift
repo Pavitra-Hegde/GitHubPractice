607d57cccff990a446451d6ac17cb156a3ddee36
Pavitra Hegde <pavitra.hegde@infanion.com>
2018-12-06 22:47:19 +0530

    Initial commit

---
+++ GitExample/GitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Pavitra Hegde on 06/12/18.
+//  Copyright © 2018 Pavitra Hegde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
